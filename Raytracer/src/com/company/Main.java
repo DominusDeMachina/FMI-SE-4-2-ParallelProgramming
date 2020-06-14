4ea571df1c0d6b1f7ec8cc02db76d55b45c6bc2f
Gennadiy Furduy <gfurduy@profitstars.com>
2020-06-14 10:36:19 +0300

    Add Java raytracer project

---
+++ Raytracer/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var startTime = System.nanoTime();
+
+        var width = 640;
+        var height = 480;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // add pixel color
+            }
+        }
+    }
+}
