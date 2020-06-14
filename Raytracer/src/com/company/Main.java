2fafa46b034da165b6c0a87214a7fcb45d82d302
Gennadiy Furduy <gfurduy@profitstars.com>
2020-06-14 15:03:07 +0300

    Make projection of sphere

--- Raytracer/src/com/company/Main.java
+++ Raytracer/src/com/company/Main.java
@@ -11,13 +11,11 @@ import com.company.primitives.Vector3D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         final long startTime = System.nanoTime();
-        Random random = new Random();
 
         final int width = 1600;
         final int height = 900;
@@ -25,12 +23,17 @@ public class Main {
         final File traceResultFile = new File("TraceResult.png");
         final BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        Sphere sphere1 = new Sphere(new Point3D(0.0,0.0,0.0), 60.0, new Color(1.0F, 0.0F, 0.0F));
+        Sphere sphere1 = new Sphere(new Point3D(0.0, 0.0, 0.0), 60.0, new Color(1.0F, 0.0F, 0.0F));
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                Ray ray = new Ray(new Point3D(x-width/2+.5, y-height/2+.5, 100), new Vector3D(0.0,0.0,-1.0));
-                bufferedImage.setRGB(x,y,random.nextInt());
+                Ray ray = new Ray(new Point3D(x - width / 2 + .5, y - height / 2 + .5, 100),
+                        new Vector3D(0.0, 0.0, -1.0));
+                if (sphere1.intersect(ray) != 0.0) {
+                    bufferedImage.setRGB(x, y, sphere1.color.toInteger());
+                } else {
+                    bufferedImage.setRGB(x, y, 0);
+                }
             }
         }
 
@@ -43,6 +46,6 @@ public class Main {
 
         final long endTime = System.nanoTime();
 
-        System.out.println("Work time: " + (endTime-startTime)/1000000000.0F);
+        System.out.println("Work time: " + (endTime - startTime) / 1000000000.0F);
     }
 }
