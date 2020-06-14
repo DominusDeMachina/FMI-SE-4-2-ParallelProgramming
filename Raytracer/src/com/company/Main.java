e673694b19cf35e5ed115c62e66dac6112654bec
Gennadiy Furduy <gfurduy@profitstars.com>
2020-06-14 10:52:59 +0300

    Generate random image

--- Raytracer/src/com/company/Main.java
+++ Raytracer/src/com/company/Main.java
@@ -1,19 +1,34 @@
 package com.company;
 
-import java.util.Arrays;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
         final long startTime = System.nanoTime();
+        Random random = new Random();
 
         int width = 640;
         int height = 480;
 
+        File traceResultFile = new File("TraceResult.png");
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                // add pixel color
+                bufferedImage.setRGB(x,y,random.nextInt());
             }
+        }
+
+        try {
+            ImageIO.write(bufferedImage, "PNG", traceResultFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
         }
 
         final long endTime = System.nanoTime();
