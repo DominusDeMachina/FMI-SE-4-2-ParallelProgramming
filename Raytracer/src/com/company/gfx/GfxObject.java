1d7ff740d6fd28cb3c33bf89d0873be27bfbec49
Gennadiy Furduy <gfurduy@profitstars.com>
2020-06-19 15:01:10 +0300

    From scratch

--- Raytracer/src/com/company/gfx/GfxObject.java
+++
@@ -1,9 +0,0 @@
-package com.company.gfx;
-
-import com.company.primitives.Color;
-import com.company.primitives.Ray;
-
-public abstract class GfxObject {
-  public Color color;
-  public abstract double intersect(Ray ray);
-}
